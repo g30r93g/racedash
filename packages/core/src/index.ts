5ba835c332025706ff90e8a4920b1de1a976069e
George Nick Gorzynski <georgegorzynski@me.com>
2026-03-07 22:39:35 +0000

    feat(core): add shared TypeScript types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,22 @@
-export {}
+export interface Lap {
+  number: number
+  lapTime: number      // individual lap duration in seconds
+  cumulative: number   // sum of all laps up to and including this one
+}
+
+export interface LapTimestamp {
+  lap: Lap
+  ytSeconds: number    // seconds from video start to this lap's START
+}
+
+export interface SessionData {
+  driver: { kart: string; name: string }
+  laps: Lap[]
+  timestamps: LapTimestamp[]
+}
+
+export interface OverlayProps {
+  session: SessionData
+  fps: number
+  durationInFrames: number
+}
