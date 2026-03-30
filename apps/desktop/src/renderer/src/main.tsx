f32a875e4e66e4f5575da6a92278bc978f9a5156
George Nick Gorzynski <georgegorzynski@me.com>
2026-03-30 22:13:00 +0100

    feat(desktop): add shadcn/ui Sonner Toaster to app root (RD-232)

--- apps/desktop/src/renderer/src/main.tsx
+++ apps/desktop/src/renderer/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './styles/global.css'
 import { App } from './App'
 import { RaceDashClerkProvider } from './provider/ClerkProvider'
+import { Toaster } from '@/components/ui/sonner'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RaceDashClerkProvider>
       <App />
+      <Toaster />
     </RaceDashClerkProvider>
   </React.StrictMode>,
 )
